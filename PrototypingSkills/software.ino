b421543858797f7a8c38971b880d870fcae14a95
Mark Palmeri <mlp6@duke.edu>
2018-11-05 15:58:24 -0500

    add software pseudo-code polish, motivated by Sam, Mary and Adarsh

--- PrototypingSkills/software.ino
+++ PrototypingSkills/software.ino
@@ -7,8 +7,7 @@
 // global variables representing "state" across all functions
 // operating mode: 0 - off, 1 - bright, 2 - mid-bright, 3 - dim, 4 - flash
 int operating_mode = 0;
-// int button_pushed = 0;
-bool button_pushed = TRUE;
+bool BUTTON_PUSHED = false;
 int previous_button_state = 0;
 int PWM_OUT = 0;
 
@@ -32,7 +31,7 @@ void loop(){
 
 }
 
-void set_pwn_based_on_operating_mode() {
+void set_pwm_based_on_operating_mode() {
 
     switch (operating_mode) {
         case 0:
@@ -50,19 +49,24 @@ void set_pwn_based_on_operating_mode() {
 }
 
 void button_pushed() {
-    BUTTON_PUSHED = 1;
-    // BUTTON_PUSHED = TRUE;
+    BUTTON_PUSHED = true;
 }
 
 void flash_the_light() {
 
 }
 
-void shine_led(PWN_OUT) {
-    digitalOutput(magic_in_here);
+void shine_led() {
+    analogWrite(PWM_LED_OUT, PWM_OUT);
 }
 
 void check_button_press() {
-    // set operating_mode
-    // reset previous button press state, interrupt catch, etc.
+    if (BUTTON_PUSHED == true) {
+        operating_mode = operating_mode + 1;
+        if (operating_mode == 5) {
+            operating_mode = 0;
+        }
+    }
+
+    BUTTON_PUSHED = false
 }
